718c52d8450eaf1925f8575df17e8fb6a2b63f4b
Owain Macbook <owain.jones74@gmail.com>
2024-08-06 23:06:46 +0100

    show button & controller on banner

--- resources/lang/en/gaze.php
+++ resources/lang/en/gaze.php
@@ -4,4 +4,7 @@ return [
     'banner_text' => 'This page is currently being viewed by :viewers.',
     'banner_text_other' => 'This page is currently being viewed by :viewers and :count other.',
     'banner_text_others' => 'This page is currently being viewed by :viewers and :count others.',
+
+    'lock_is_controller' => 'You currently have control of this page.',
+    'lock_user_controller' => ':name currently has control of this page.',
 ];
--- resources/views/forms/components/gaze-banner.blade.php
+++ resources/views/forms/components/gaze-banner.blade.php
@@ -5,18 +5,44 @@
 >
     @if($show)
         <div>
-            <div class="flex flex-row gap-2 rounded-xl p-6 border border-primary-500 text-primary-500 bg-gray-100 dark:bg-gray-900">
+            <div class="flex flex-row gap-4 rounded-xl p-6 border border-primary-500 text-primary-500 bg-gray-100 dark:bg-gray-900">
                 <div class="flex flex-col justify-center">
-                    <x-filament::icon
-                        alias="panels::filament-gaze.banner.icon"
-                        icon="heroicon-m-eye"
-                        class="h-5 w-5 my-auto"
-                    />
+                    @if($isLockable && !$hasControl)
+                        <x-filament::icon
+                            alias="panels::filament-gaze.banner.icon"
+                            icon="heroicon-m-lock-closed"
+                            class="h-5 w-5 my-auto"
+                        />
+                    @else
+                        <x-filament::icon
+                            alias="panels::filament-gaze.banner.icon"
+                            icon="heroicon-m-eye"
+                            class="h-5 w-5 my-auto"
+                        />
+                    @endif
                 </div>
 
-                <p>
-                    {{ $text }}
-                </p>
+                <div class="flex flex-col md:flex-row w-full justify-between">
+                    <div>
+                        <p>{{ $text }}</p>
+                        @if($isLockable)
+                            @if($hasControl)
+                                <p>{{ __('filament-gaze::gaze.lock_is_controller') }}</p>
+                            @else
+                                <p>{{ __('filament-gaze::gaze.lock_user_controller', ['name' => $controlUser['name']]) }}</p>
+                            @endif
+
+                            @if($canTakeControl && !$hasControl)
+                            @endif
+                        @endif
+                    </div>
+
+                    @if($isLockable && !$hasControl && $canTakeControl)
+                        <x-filament::button class="my-auto mt-2 md:mt-0" color="primary">
+                            Take Control
+                        </x-filament::button>
+                    @endif
+                </div>
             </div>
         </div>
     @endif
