e50a2ce07f3635faa198eacd2d652dcedf1c07e0
Creekie <hi@creekie.me>
2024-09-02 23:24:34 +0200

    Dutch Translations

---
+++ resources/lang/nl/gaze.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'banner_text' => 'Deze pagina wordt momenteel bekeken door :viewers.',
+    'banner_text_other' => 'Deze pagina wordt momenteel bekeken door :viewers en :count anderen.',
+    'banner_text_others' => 'Deze pagina wordt momenteel bekeken door :viewers en :count anderen.',
+
+    'lock_is_controller' => 'Je hebt momenteel controle over deze pagina.',
+    'lock_user_controller' => ':name heeft momenteel controle over deze pagina.',
+    'lock_take_control' => 'Controle nemen',
+];
