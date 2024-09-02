af100bc74eebfddf4aedfb2c8c6b93fcabdc7fce
Andréas Lundgren <andreas.lundgren@halle.se>
2024-09-02 16:39:03 +0200

    Add Swedish translations

---
+++ resources/lang/sv/gaze.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'banner_text' => 'Den här sidan visas just nu av :viewers.',
+    'banner_text_other' => 'Den här sidan visas just nu av :viewers och :count till.',
+    'banner_text_others' => 'Den här sidan visas just nu av :viewers och :count andra.',
+
+    'lock_is_controller' => 'Du har kontroll över den här sidan.',
+    'lock_user_controller' => ':name har kontroll över den här sidan.',
+    'lock_take_control' => 'Ta kontroll',
+];
