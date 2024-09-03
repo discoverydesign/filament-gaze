3ee43f6334704b4754a6df39f5d2168ea16d9cec
Arne Becker <arbecker@stroeer.de>
2024-09-03 15:14:41 +0200

    german translation

---
+++ resources/lang/de/gaze.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'banner_text' => 'Diese Seite wird derzeit von :viewers angesehen.',
+    'banner_text_other' => 'Diese Seite wird derzeit von :viewers und :count weiteren angesehen.',
+    'banner_text_others' => 'Diese Seite wird derzeit von :viewers und :count weiteren angesehen.',
+
+    'lock_is_controller' => 'Sie haben derzeit die Kontrolle über diese Seite.',
+    'lock_user_controller' => ':name hat derzeit die Kontrolle über diese Seite.',
+    'lock_take_control' => 'Kontrolle übernehmen',
+];
