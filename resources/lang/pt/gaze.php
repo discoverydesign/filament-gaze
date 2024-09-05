052c4188efbbea39600e78dd771f194c05e2fd26
a21ns1g4ts <atila.danvi@outlook.com>
2024-09-05 06:44:14 -0300

    Add pt translations

---
+++ resources/lang/pt/gaze.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'banner_text' => 'Esta página está sendo visualizada por :viewers.',
+    'banner_text_other' => 'Esta página está sendo visualizada por :viewers e mais :count pessoa.',
+    'banner_text_others' => 'Esta página está sendo visualizada por :viewers e mais :count pessoas.',
+
+    'lock_is_controller' => 'Você atualmente tem o controle desta página.',
+    'lock_user_controller' => ':name atualmente tem o controle desta página.',
+    'lock_take_control' => 'Assumir Controle',
+];
