2d7a6ddd51ee2d1202d7580c7b812297b5231bf3
HAMAD ABDULLA <bo3bdo@hotmail.com>
2024-11-23 21:40:19 +0300

    add arabic language

---
+++ resources/lang/ar/gaze.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'banner_text' => 'هذه الصفحة يتم عرضها حالياً بواسطة :viewers.',
+    'banner_text_other' => 'هذه الصفحة يتم عرضها حالياً بواسطة :viewers و :count آخر.',
+    'banner_text_others' => 'هذه الصفحة يتم عرضها حالياً بواسطة :viewers و :count آخرين.',
+
+    'lock_is_controller' => 'لديك حالياً التحكم في هذه الصفحة.',
+    'lock_user_controller' => ':name لديه حالياً التحكم في هذه الصفحة.',
+    'lock_take_control' => 'تولي التحكم',
+];
