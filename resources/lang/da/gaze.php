c2ed007ce16141054a7d3bb8c8cce73b09ac76fb
Mathias Balsløw <49169557+mbalslow@users.noreply.github.com>
2025-01-18 21:11:26 +0100

    Add Danish language file

---
+++ resources/lang/da/gaze.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'banner_text' => 'Denne side bliver i øjeblikket vist af :viewers.',
+    'banner_text_other' => 'Denne side bliver i øjeblikket vist af :viewers og :count anden.',
+    'banner_text_others' => 'Denne side bliver i øjeblikket vist af :viewers og :count andre.',
+
+    'lock_is_controller' => 'Du har i øjeblikket styring af denne side.',
+    'lock_user_controller' => ':name har i øjeblikket styring af denne side.',
+    'lock_take_control' => 'Tag styringen',
+];
