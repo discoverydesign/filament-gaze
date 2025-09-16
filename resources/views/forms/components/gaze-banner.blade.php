5efd8b617154654ab33b85987810d469a6aae9d3
Owain <owain.jones74@gmail.com>
2025-09-16 22:05:04 +0100

    move to custom css classes

--- resources/views/forms/components/gaze-banner.blade.php
+++ resources/views/forms/components/gaze-banner.blade.php
@@ -1,27 +1,32 @@
+@php
+    use Filament\Support\Icons\Heroicon;
+    use Filament\Support\Enums\IconSize;
+@endphp
+
 <div
     class="gaze-banner @if($show) gaze-banner--has-content @endif"
     x-data="{}"
     wire:poll.keep-alive.{{ $pollTimer }}s="$dispatch('FilamentGaze::refreshViewers')"
 >
     @if($show)
-        <div class="flex flex-row items-center gap-4 rounded-xl p-6 border border-primary-500 text-primary-500 bg-gray-100 dark:bg-gray-900">
-            <div class="flex-shrink-0">
+        <div class="fi-gaze-banner">
+            <div class="icon-container">
                 @if($isLockable && !$hasControl)
                     <x-filament::icon
                         alias="filament-gaze::banner.locked"
-                        icon="heroicon-m-lock-closed"
-                        class="h-5 w-5"
+                        :icon="Heroicon::LockClosed"
+                        :size="IconSize::Large"
                     />
                 @else
                     <x-filament::icon
                         alias="filament-gaze::banner.view"
-                        icon="heroicon-m-eye"
-                        class="h-5 w-5"
+                        :icon="Heroicon::Eye"
+                        :size="IconSize::Large"
                     />
                 @endif
             </div>
 
-            <div class="flex flex-col flex-1">
+            <div class="text-container">
                 <p>{{ $text }}</p>
                 @if($isLockable)
                     @if($hasControl)
@@ -33,7 +38,7 @@
             </div>
 
             @if($isLockable && !$hasControl && $canTakeControl)
-                <x-filament::button class="flex-shrink-0" color="primary" wire:click="$dispatch('FilamentGaze::takeControl')">
+                <x-filament::button class="button-container" color="primary" wire:click="$dispatch('FilamentGaze::takeControl')">
                     {{ __('filament-gaze::gaze.lock_take_control') }}
                 </x-filament::button>
             @endif
